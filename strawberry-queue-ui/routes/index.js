1e3d82e32b8d74393b14d7633b0e98ffd53f411c
RBeschastnyh <Drean_page@mail.ru>
2020-03-21 01:22:42 +0300

    UI canvas has been updated

--- strawberry-queue-ui/routes/index.js
+++ strawberry-queue-ui/routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Привет! Чо, в очередь записываешься?' });
 });
+
+router.get('/complete', function(req, res, next){
+    res.render('book')
+});
+
+router.post('/book', function(req, res, next) {
+      request.post({
+        url: 'http://localhost:8081/book',
+        form: {
+          desiredDate: req.body.data_space,
+          desiredTask: req.body.class_space,
+          studentName: req.body.name_space,
+          subject: "string"
+        }
+      }, (err, response, body) =>{
+        if(err) {
+          return res.status(500).send({message: err});
+        }
+        return res.send(body);
+      });
+    }
+);
 
 module.exports = router;
