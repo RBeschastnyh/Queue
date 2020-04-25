eebf1e55deb6d21ca389fc832ff04b8806e8cb3b
RBeschastnyh <Drean_page@mail.ru>
2020-04-26 00:25:56 +0300

    Frontend part has been updated

--- strawberry-queue-ui/routes/index.js
+++ strawberry-queue-ui/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Привет! Чо, в очередь записываешься?' });
+  res.render('index', { title: 'Очередь СКБ' });
 });
 
 router.get('/complete', function(req, res, next){
@@ -15,16 +15,16 @@ router.post('/book', function(req, res, next) {
       request.post({
         url: 'http://localhost:8081/book',
         form: {
-          desiredDate: req.body.data_space,
-          desiredTask: req.body.class_space,
+          desiredDate: req.body.data_space.toString(),
+          desiredTask: req.body.task_space,
           studentName: req.body.name_space,
-          subject: "string"
+          subject: req.body.class_space
         }
       }, (err, response, body) =>{
         if(err) {
           return res.status(500).send({message: err});
         }
-        return res.send(body);
+        result = res.send(body);
       });
     }
 );
